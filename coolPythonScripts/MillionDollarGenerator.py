0ffd13eddfb74959505fd59f194792aa2700ecfe
Emil Åkesson <emak0001@student.hv.se>
2021-12-07 13:12:09 +0100

    Added new functionality and changed earlier funtion with two variable input in function instead

--- coolPythonScripts/MillionDollarGenerator.py
+++ coolPythonScripts/MillionDollarGenerator.py
@@ -1,6 +1,16 @@
 money = 0
 
-def createMoney():
-	for i in range(10):
+
+def createMoney(n):
+	for i in range(n):
 		money = money +1
 //Vi behöver printa dem också..
+
+
+def write_hello(antal, namn):
+	money = createMoney(antal)
+	print(namn + "har: " + money + " kr."
+		
+
+write_hello(10,"Emil") 
+	
