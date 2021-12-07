94a2418f2cd29050502cf4e4cbd531f94ea9dc5e
Emil <emil_gebl@hotmail.com>
2021-12-07 13:11:15 +0100

    Fixade scriptet, fanns några fel.. typ

--- coolPythonScripts/MillionDollarGenerator.py
+++ coolPythonScripts/MillionDollarGenerator.py
@@ -1,6 +1,16 @@
 money = 0
+bool enterTheFramework = False
 
 def createMoney():
 	for i in range(10):
 		money = money +1
+	return
 //Vi behöver printa dem också..
+//
+//
+//
+//
+def LimitationBypass():
+	enterTheFramework = True:
+	return
+//Ingen main fil ??
